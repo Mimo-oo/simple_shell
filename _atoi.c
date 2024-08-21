34e845b6c9f88b53ec5ce355d0d6d6b330a31662
mimo-oo <fadipemichael6@gmail.com>
2024-08-21 13:26:55 -0700

    delimiter

--- _atoi.c
+++ _atoi.c
@@ -11,3 +11,17 @@ int in_interactive(info_m *info)
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
+/**
+ * is_delimiter - checks if character is a delimeter
+ * @c: the char to check
+ * @delimit: the delimeter string
+ * Return: 1 if true, 0 if false
+ */
+int is_delimiter(char b, char *delimit)
+{
+	while (*delimit)
+		if (*delimit++ == b)
+			return (1);
+	return (0);
+}
+
