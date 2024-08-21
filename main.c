3860f4a15d8f41e1d97e137f016d16b80ea76f3e
mimo-oo <fadipemichael6@gmail.com>
2024-08-21 12:40:45 -0700

    main

--- main.c
+++ main.c
@@ -11,17 +11,17 @@
 int main(int ac, char **av)
 {
 	info_m info[] = { INFO_INITIALIZE };
-	int filed = 2;
+	int fdisc = 2;
 
 	asm ("mov %1, %0\n\t"
 		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
+		: "=r" (fdisc)
+		: "r" (fdisc));
 
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
+		if (fdisc == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
@@ -36,13 +36,10 @@ int main(int ac, char **av)
 			}
 			return (EXIT_FAILURE);
 		}
-		info->readfd = fd;
+		info->readfd = fdisc;
 	}
 	populate_env_list(info);
 	read_history(info);
 	hsh(info, av);
 	return (EXIT_SUCCESS);
 }
-
-
-
