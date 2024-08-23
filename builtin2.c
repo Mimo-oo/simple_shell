1a0ffc89bed12b86828db3acaf24607e4ebc242d
mimo-oo <fadipemichael6@gmail.com>
2024-08-23 16:18:19 -0700

    buitlin

--- builtin2.c
+++ builtin2.c
@@ -82,3 +82,37 @@ int print_alias(list_t *node)
 	return (1);
 }
 
+/**
+ * _myownalias - same as the alias builtin -man alias
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: 0 on success
+ */
+int _myownalias(info_m *info)
+{
+	int i = 0;
+	char *h = NULL;
+	list_t *node = NULL;
+
+	if (info->argc == 1)
+	{
+		node = info->alias;
+		while (node)
+		{
+			print_alias(node);
+			node = node->next;
+		}
+		return (0);
+	}
+	for (i = 1; info->argv[i]; i++)
+	{
+		h = _strchr(info->argv[i], '=');
+		if (h)
+			set_alias(info, info->argv[i]);
+		else
+			print_alias(node_starts_with(info->alias, info->argv[i], '='));
+	}
+
+	return (0);
+}
+
