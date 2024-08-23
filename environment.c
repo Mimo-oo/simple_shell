73b47172248955a85570fcae7f74e759582ec428
mimo-oo <fadipemichael6@gmail.com>
2024-08-23 16:26:34 -0700

    env

--- environment.c
+++ environment.c
@@ -12,3 +12,25 @@ int _myenv(info_m *info)
 	return (0);
 }
 
+/**
+ * _getdenv - gets the value of an environ variable
+ * @info: Structure containing potential arguments. Used to maintain
+ * @name: env var name
+ *
+ * Return: the value
+ */
+char *_getdenv(info_m *info, const char *name)
+{
+	list_t *node = info->env;
+	char *h;
+
+	while (node)
+	{
+		h = starts_with(node->str, name);
+		if (h && *h)
+			return (h);
+		node = node->next;
+	}
+	return (NULL);
+}
+
