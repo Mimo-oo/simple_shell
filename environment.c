412d178aaa3517ce36120e1f9dd2107292b6945c
mimo-oo <fadipemichael6@gmail.com>
2024-08-24 06:59:13 -0700

    environment

--- environment.c
+++ environment.c
@@ -74,3 +74,20 @@ int _myownunsetenv(info_m *info)
 	return (0);
 }
 
+/**
+ * populates_env_list - populates env linked list
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+int populates_env_list(info_m *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
+	return (0);
+}
+
