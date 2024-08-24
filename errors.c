1af790802c3ea3d545ac748ee14e6accd2617a7a
mimo-oo <fadipemichael6@gmail.com>
2024-08-24 07:14:58 -0700

    error

--- errors.c
+++ errors.c
@@ -19,3 +19,25 @@ void _eputs(char *str)
 	}
 }
 
+/**
+ * _dputchar - writes the character c to stderr
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _dputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUFFER_SIZE];
+
+	if (c == BUFFER_FLUSH || i >= WRITE_BUFFER_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUFFER_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
