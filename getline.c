c29d19d743b4cec853538b16335e75b5482e4aa1
mimo-oo <fadipemichael6@gmail.com>
2024-08-26 14:09:57 -0700

    get line

--- getline.c
+++ getline.c
@@ -42,3 +42,48 @@ ssize_t input_buff(info_m *info, char **buf, size_t *len)
 	return (r);
 }
 
+}
+
+
+/**
+ * input_buff - buffers chained commands
+ * @info: parameter struct
+ * @buf: address of buffer
+ * @len: address of len var
+ *
+ * Return: bytes read
+ */
+ssize_t input_buff(info_m *info, char **buf, size_t *len)
+{
+	ssize_t r = 0;
+	size_t len_p = 0;
+
+	if (!*len)
+	{
+		free(*buf);
+		*buf = NULL;
+		signal(SIGINT, sigint_Handler);
+#if USE_GETLINE
+		r = getline(buf, &len_p, stdin);
+#else
+		r = _getline(info, buf, &len_p);
+#endif
+		if (r > 0)
+		{
+			if ((*buf)[r - 1] == '\n')
+			{
+				(*buf)[r - 1] = '\0'; /* remove trailing newline */
+				r--;
+			}
+			info->linecount_flag = 1;
+			removes_comment(*buf);
+			build_history_list(info, *buf, info->histcount++);
+			{
+				*len = r;
+				info->cmd_buf = buf;
+			}
+		}
+	}
+	return (r);
+}
+
