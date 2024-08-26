edf621975c53d8ef33b07706600fac812d453f17
mimo-oo <fadipemichael6@gmail.com>
2024-08-26 13:37:31 -0700

    exit

--- errors2.c
+++ errors2.c
@@ -85,3 +85,39 @@ int print_desc(int input, int fd)
 	return (count);
 }
 
+/**
+ * convert_numbers - converter function, a clone of atoi
+ * @num: number
+ * @base: base
+ * @flags: argument flags
+ *
+ * Return: string
+ */
+char *convert_numbers(long int num, int base, int flags)
+{
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do	{
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
+
