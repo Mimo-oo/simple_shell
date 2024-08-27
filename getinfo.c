b45138995a406060002c6e474381f259ffd63812
mimo-oo <fadipemichael6@gmail.com>
2024-08-27 14:38:08 -0700

    get info

--- getinfo.c
+++ getinfo.c
@@ -12,3 +12,35 @@ void clear_infos(info_m *info)
 	info->argc = 0;
 }
 
+/**
+ * set_infos - initializes info_m struct
+ * @info: struct address
+ * @av: argument vector
+ */
+void set_infos(info_m *info, char **av)
+{
+	int i = 0;
+
+	info->fname = av[0];
+	if (info->arg)
+	{
+		info->argv = strtow(info->arg, " \t");
+		if (!info->argv)
+		{
+
+			info->argv = malloc(sizeof(char *) * 2);
+			if (info->argv)
+			{
+				info->argv[0] = _strdup(info->arg);
+				info->argv[1] = NULL;
+			}
+		}
+		for (i = 0; info->argv && info->argv[i]; i++)
+			;
+		info->argc = i;
+
+		replace_alias(info);
+		replace_vars(info);
+	}
+}
+
