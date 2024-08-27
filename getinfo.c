38744a42d1af2c8859dddd76944e23b2577a0e6e
mimo-oo <fadipemichael6@gmail.com>
2024-08-27 15:16:09 -0700

    get info

--- getinfo.c
+++ getinfo.c
@@ -59,11 +59,11 @@ void free_infos(info_m *info, int all)
 		if (!info->cmd_buf)
 			free(info->arg);
 		if (info->env)
-			free_list(&(info->env));
+			frees_list(&(info->env));
 		if (info->history)
-			free_list(&(info->history));
+			frees_list(&(info->history));
 		if (info->alias)
-			free_list(&(info->alias));
+			frees_list(&(info->alias));
 		f_free(info->environ);
 			info->environ = NULL;
 		b_free((void **)info->cmd_buf);
