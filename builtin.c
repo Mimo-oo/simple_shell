6d5adad6dde46f5793733d00e76fdafdae28d5d2
mimo-oo <fadipemichael6@gmail.com>
2024-08-27 14:30:05 -0700

    built in

--- builtin.c
+++ builtin.c
@@ -73,8 +73,8 @@ int _chmycd(info_m *info)
 	}
 	else
 	{
-		_setenv(info, "FMRPWD", _getdenv(info, "PWD="));
-		_setenv(info, "PWD", getcwd(buffer, 1024));
+		_setenvir(info, "FMRPWD", _getdenv(info, "PWD="));
+		_setenvir(info, "PWD", getcwd(buffer, 1024));
 	}
 	return (0);
 }
