1f4dc224bfd220d3ee94f99f2251e81e3e2ceb12
mimo-oo <fadipemichael6@gmail.com>
2024-08-27 14:32:14 -0700

    enviro

--- environment.c
+++ environment.c
@@ -48,7 +48,7 @@ int _myownsetenv(info_m *info)
 		_eputs("Incorrect number of arguments\n");
 		return (1);
 	}
-	if (_setenv(info, info->argv[1], info->argv[2]))
+	if (_setenvir(info, info->argv[1], info->argv[2]))
 		return (0);
 	return (1);
 }
