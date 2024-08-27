4371ff38e50b9f0db994c387b5fd06bdbca38b42
mimo-oo <fadipemichael6@gmail.com>
2024-08-27 14:19:07 -0700

    get envi

--- getenvironment.c
+++ getenvironment.c
@@ -17,3 +17,35 @@ char **get_environment(info_m *info)
 	return (info->environ);
 }
 
+/**
+ * _unsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: 1 on delete, 0 otherwise
+ * @var: the string env var property
+ */
+int _unsetenv(info_m *info, char *var)
+{
+	list_t *node = info->env;
+	size_t i = 0;
+	char *d;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
+	{
+		d = starts_with(node->str, var);
+		if (d && *d == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), i);
+			i = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (info->env_changed);
+}
+
