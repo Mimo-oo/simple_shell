e9f4f8c7d8f42da75ade01aaa1ea917b3068e64d
mimo-oo <fadipemichael6@gmail.com>
2024-08-27 14:28:05 -0700

    get envi

--- getenvironment.c
+++ getenvironment.c
@@ -49,3 +49,46 @@ int _unsetenvv(info_m *info, char *var)
 	return (info->env_changed);
 }
 
+/**
+ * _setenvir - Initialize a new environment variable,
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ * @var: the string env var property
+ * @value: the string env var value
+ *  Return: Always 0
+ */
+int _setenvir(info_m *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *d;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
+	{
+		d = starts_with(node->str, var);
+		if (d && *d == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
+	return (0);
+}
+
