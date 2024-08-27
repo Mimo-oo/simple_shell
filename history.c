6bfd3d01f7cbf6fcec73899b379f13ef3e2661ee
mimo-oo <fadipemichael6@gmail.com>
2024-08-27 14:49:32 -0700

    history

--- history.c
+++ history.c
@@ -22,3 +22,32 @@ char *g_history_file(info_m *info)
 	return (buf);
 }
 
+/**
+ * write_history - creates a file, or appends to an existing file
+ * @info: the parameter struct
+ *
+ * Return: 1 on success, else -1
+ */
+int write_history(info_m *info)
+{
+	ssize_t fd;
+	char *filename = g_history_file(info);
+	list_t *node = NULL;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
+	free(filename);
+	if (fd == -1)
+		return (-1);
+	for (node = info->history; node; node = node->next)
+	{
+		_putsfdesc(node->str, fd);
+		_putfdesc('\n', fd);
+	}
+	_putfdesc(BUFFER_FLUSH, fd);
+	close(fd);
+	return (1);
+}
+
