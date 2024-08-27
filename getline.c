c1fe9bfa79f7e0c07a19da094a505408bfc855dc
mimo-oo <fadipemichael6@gmail.com>
2024-08-27 13:56:55 -0700

    get line

--- getline.c
+++ getline.c
@@ -87,3 +87,23 @@ ssize_t get_inputs(info_m *info)
 	return (r);
 }
 
+/**
+ * read_buf - reads a buffer
+ * @info: parameter struct
+ * @buf: buffer
+ * @i: size
+ *
+ * Return: r on success
+ */
+ssize_t read_buf(info_m *info, char *buf, size_t *i)
+{
+	ssize_t r = 0;
+
+	if (*i)
+		return (0);
+	r = read(info->readfd, buf, READ_BUFFER_SIZE);
+	if (r >= 0)
+		*i = r;
+	return (r);
+}
+
