1bddafda9659f983faab414af6ec462b03902b6e
mimo-oo <fadipemichael6@gmail.com>
2024-08-28 11:07:57 -0700

    var

--- var.c
+++ var.c
@@ -98,3 +98,44 @@ int replace_alias(info_m *info)
 	return (1);
 }
 
+/**
+ * replace_vars - replaces vars in the tokenized string
+ * @info: the parameter struct
+ *
+ * Return: 1 if replaced, 0 otherwise
+ */
+int replace_vars(info_m *info)
+{
+	int i = 0;
+	list_t *node;
+
+	for (i = 0; info->argv[i]; i++)
+	{
+		if (info->argv[i][0] != '$' || !info->argv[i][1])
+			continue;
+
+		if (!_strcmp(info->argv[i], "$?"))
+		{
+			replaces_strings(&(info->argv[i]),
+				_strdup(convert_numbers(info->status, 10, 0)));
+			continue;
+		}
+		if (!_strcmp(info->argv[i], "$$"))
+		{
+			replaces_strings(&(info->argv[i]),
+				_strdup(convert_numbers(getpid(), 10, 0)));
+			continue;
+		}
+		node = node_starts_with_p(info->env, &info->argv[i][1], '=');
+		if (node)
+		{
+			replaces_strings(&(info->argv[i]),
+				_strdup(_strchr(node->str, '=') + 1));
+			continue;
+		}
+		replaces_strings(&info->argv[i], _strdup(""));
+
+	}
+	return (0);
+}
+
