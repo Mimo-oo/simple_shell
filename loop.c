8f5e648efa83d20dd251b09c67ce46c66022ff23
mimo-oo <fadipemichael6@gmail.com>
2024-08-28 10:40:10 -0700

    loop

--- loop.c
+++ loop.c
@@ -43,3 +43,37 @@ int hsh(info_m *info, char **av)
 	return (builtin_ret);
 }
 
+/**
+ * find_builtins - finds builtin commands
+ * @info: the parameter & return info struct
+ *
+ * Return: -1 if builtin not found,
+ *			0 if builtin executed successfully,
+ *			1 if builtin found but not successful,
+ *			-2 if builtin signals exit()
+ */
+int find_builtins(info_m *info)
+{
+	int i, built_in_ret = -1;
+	builtin_table builtintbl[] = {
+		{"exit", _exiting},
+		{"env", _myenv},
+		{"help", _needhelp},
+		{"history", _thehistory},
+		{"setenv", _myownsetenv},
+		{"unsetenv", _myownunsetenv},
+		{"cd", _chmycd},
+		{"alias", _myownalias},
+		{NULL, NULL}
+	};
+
+	for (i = 0; builtintbl[i].type; i++)
+		if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
+		{
+			info->line_count++;
+			built_in_ret = builtintbl[i].func(info);
+			break;
+		}
+	return (built_in_ret);
+}
+
