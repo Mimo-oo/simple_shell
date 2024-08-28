0e14a22176e07610df0d08986289009bcd574024
mimo-oo <fadipemichael6@gmail.com>
2024-08-28 10:46:04 -0700

    string

--- string.c
+++ string.c
@@ -18,3 +18,25 @@ int _strlen(char *s)
 	return (i);
 }
 
+/**
+ * _strcmp - performs lexicographic comparison of two strings.
+ * @s1: the first strang
+ * @s2: the second strang
+ *
+ * Return: - if s1 < s2, + if s1 > s2, 0 if s1 == s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
