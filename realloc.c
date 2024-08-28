ca1dcc5ea50e645bcb03f75d00d3db71b4ab175f
mimo-oo <fadipemichael6@gmail.com>
2024-08-28 10:37:46 -0700

    realloc

--- realloc.c
+++ realloc.c
@@ -31,3 +31,33 @@ void f_free(char **pp)
 	free(a);
 }
 
+/**
+ * _realloc - reallocates a block of memory
+ * @ptr: pointer to previous m-allocate block
+ * @old_size: byte size of previous block
+ * @new_size: byte size of new block
+ *
+ * Return: a pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *h;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
+
+	h = malloc(new_size);
+	if (!h)
+		return (NULL);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		h[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (h);
+}
+
