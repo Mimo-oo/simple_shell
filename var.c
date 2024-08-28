26cdbc4e8fe4ae01ff9a9e62b54e06a7a8fdb9e5
mimo-oo <fadipemichael6@gmail.com>
2024-08-28 11:05:59 -0700

    var

--- var.c
+++ var.c
@@ -35,3 +35,37 @@ int is_chain(info_m *info, char *buf, size_t *p)
 	return (1);
 }
 
+/**
+ * checks_chain - checks we should continue chaining based on last status
+ * @info: the parameter struct
+ * @buf: the char buffer
+ * @p: address of current position in buf
+ * @i: starting position in buf
+ * @len: length of buf
+ *
+ * Return: Void
+ */
+void checks_chain(info_m *info, char *buf, size_t *p, size_t i, size_t len)
+{
+	size_t j = *p;
+
+	if (info->cmd_buf_type == CMD_AND)
+	{
+		if (info->status)
+		{
+			buf[i] = 0;
+			j = len;
+		}
+	}
+	if (info->cmd_buf_type == CMD_OR)
+	{
+		if (!info->status)
+		{
+			buf[i] = 0;
+			j = len;
+		}
+	}
+
+	*p = j;
+}
+
