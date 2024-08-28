5d2bd2c20278910851cdc06c9c6358d5b9c20235
mimo-oo <fadipemichael6@gmail.com>
2024-08-28 10:43:13 -0700

    loop

--- loop.c
+++ loop.c
@@ -119,3 +119,43 @@ void find_cmd(info_m *info)
 	}
 }
 
+/**
+ * fork_cmd - forks a an exec thread to run cmd
+ * @info: the parameter & return info struct
+ *
+ * Return: void
+ */
+void fork_cmd(info_m *info)
+{
+	pid_t child_pid;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		/* TODO: put error function */
+		perror("Error:");
+		return;
+	}
+	if (child_pid == 0)
+	{
+		if (execve(info->path, info->argv, get_environment(info)) == -1)
+		{
+			free_infos(info, 1);
+			if (errno == EACCES)
+				exit(126);
+			exit(1);
+		}
+		/* TODO: put error function */
+	}
+	else
+	{
+		wait(&(info->status));
+		if (WIFEXITED(info->status))
+		{
+			info->status = WEXITSTATUS(info->status);
+			if (info->status == 126)
+				print_error(info, "Permission denied\n");
+		}
+	}
+}
+
