a1f55ebfea918ee4a4a34d47f6fc58b99dbbdf8b
mimo-oo <fadipemichael6@gmail.com>
2024-08-28 10:32:32 -0700

    memory

--- memory.c
+++ memory.c
@@ -17,3 +17,27 @@ int b_free(void **ptr)
 	return (0);
 }
 
+#include "shell.h"
+
+/**
+ * i_cmd - determines if a file is an executable command
+ * @info: the info struct
+ * @path: path to the file
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+int i_cmd(info_m *info, char *path)
+{
+	struct stat st;
+
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+
