e2b5c8ecc87ceeb502a364fb711c98a05fe0d5bf
mimo-oo <fadipemichael6@gmail.com>
2024-08-28 12:21:00 -0700

    correction

--- errors2.c
+++ errors2.c
@@ -121,3 +121,21 @@ char *convert_numbers(long int num, int base, int flags)
 	return (ptr);
 }
 
+/**
+ * removes_comment - function replaces first instance of '#' with '\0'
+ * @buf: address of the string to modify
+ *
+ * Return: Always 0;
+ */
+void removes_comment(char *buf)
+{
+	int i;
+
+	for (i = 0; buf[i] != '\0'; i++)
+		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+		{
+			buf[i] = '\0';
+			break;
+		}
+}
+
